5cbdb5063c7646db077a3637b8b9bc61474ea0dd
Rodrigo Brito <rig.10@hotmail.com>
2017-06-18 14:13:29 -0300

    Inclusão de acesso via CORS

--- src/main/java/br/com/sabaramais/onibus/config/ApplicationConfiguration.java
+++ src/main/java/br/com/sabaramais/onibus/config/ApplicationConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,6 +15,11 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 @Configuration
 @EnableWebMvc
 public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+	
 	@Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         for (HttpMessageConverter converter : converters) {
